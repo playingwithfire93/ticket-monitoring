402f11fb67d5ca4067bd98fa3ae5cb8e4d821b3d
playingwithfire93 <adatellezvalle@gmail.com>
2025-06-22 21:55:36 +0200

    Initial ticket monitor

--- app.py
+++ app.py
@@ -118,6 +118,12 @@ def get_changes():
         }
     ])
 
+@app.route("/urls")
+def urls():
+    with open("urls.json") as f:
+        data = json.load(f)
+    return jsonify(data)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
