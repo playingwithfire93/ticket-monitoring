99baebdba5230112e0cd0f1f8f1fc33af8245d0e
playingwithfire93 <adatellezvalle@gmail.com>
2025-06-22 21:32:02 +0200

    Initial ticket monitor

--- app.py
+++ app.py
@@ -20,4 +20,7 @@ def changes():
     return jsonify(data["changes"])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
