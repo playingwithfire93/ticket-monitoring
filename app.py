8612fb60a3cfce550f742cb22833988631ac01f0
playingwithfire93 <adatellezvalle@gmail.com>
2025-06-24 22:02:55 +0200

    Initial ticket monitor

--- app.py
+++ app.py
@@ -462,7 +462,7 @@ def home():
 """)
 
 URLS = [
-    {"label": "test", "url": "https://httpbin.org/get/"},
+    #{"label": "test", "url": "https://httpbin.org/get/"},
     {"label": "Wicked", "url": "https://wickedelmusical.com/"},
     {"label": "Wicked elenco", "url": "https://wickedelmusical.com/elenco"},
     {"label": "Wicked entradas", "url": "https://tickets.wickedelmusical.com/espectaculo/wicked-el-musical/W01"},
