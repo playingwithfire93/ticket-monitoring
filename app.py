f681c82ba7864774c61706baecf1efbe556c2366
playingwithfire93 <adatellezvalle@gmail.com>
2026-01-04 16:29:32 +0100

    -

--- app.py
+++ app.py
@@ -55,6 +55,9 @@ app = Flask(__name__,
 
 db_url = os.getenv('DATABASE_URL')
 if db_url:
+    # Fix for Render/Heroku: postgres:// -> postgresql://
+    if db_url.startswith('postgres://'):
+        db_url = db_url.replace('postgres://', 'postgresql://', 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(BASE / 'musicals.db')
